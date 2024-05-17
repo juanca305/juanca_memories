4f5f76dea350dcd0c7c9d4ed802f40f75b6a77de
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-05-17 07:49:42 -0400

    Added JWT logic successfully

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -60,22 +60,19 @@ const Navbar = () => {
               //<div>Logged In</div>
 
                 <div className={classes.profile}>
-                    <Avatar className={classes.purple} alt={user?.result.given_name} src={user?.result.picture}>
-                        {user?.result.given_name.charAt(0)}
+                    <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.picture}>
+                        {user?.result.name.charAt(0)}
                         
                     </Avatar>
                     <Typography className={classes.userName} variant="h6">
-                        {user?.result.given_name}
+                        {user?.result.name}
                         
                     </Typography>
                     <Button variant='contained' className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                 </div>
             ) : (
                  <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-                // <GoogleLogin
-                //   onSuccess={(response) => console.log(response)} 
-                //   onError={() => console.log('Error')}
-                // />
+               
             )}
         </Toolbar>
       </AppBar>
