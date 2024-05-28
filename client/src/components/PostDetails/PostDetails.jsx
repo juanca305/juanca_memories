c2784276c38961725d4a0669972908b9af4329c0
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-05-28 09:08:16 -0400

    Added Recommended Posts Feature

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -3,7 +3,7 @@ import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core'
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import { useParams, useHistory } from 'react-router-dom';
-import { getPost } from '../../actions/posts';
+import { getPost, getPostsBySearch } from '../../actions/posts';
 
 import useStyles from './styles';
 
@@ -19,6 +19,13 @@ const PostDetails = () => {
       dispatch(getPost(id));
     }, [id]);
 
+    useEffect(() => {
+     if(post) {
+      dispatch(getPostsBySearch({ search: 'none', tags: post?.tags.join(',')}))
+     }
+    }, [post])
+    
+
     if(!post) return null;
 
     if(isLoading) {
@@ -27,6 +34,9 @@ const PostDetails = () => {
       </Paper>
     }
     
+    const recommendedPosts = posts.filter(({ _id }) => _id !== post._id);
+
+    const openPost = (_id) => history.push(`/posts/${_id}`);
 
   return (
     // <div>
@@ -52,6 +62,23 @@ const PostDetails = () => {
           </div>
       </div>
       {/* Recommended Posts */}
+      {recommendedPosts.length && (
+        <div className={classes.section}>
+          <Typography gutterBottom variant='h5'>You might also like:</Typography>
+          <Divider />
+          <div className={classes.recommendedPosts}>
+            {recommendedPosts.map(({ title, message, name, likes, selectedFile, _id }) => (
+              <div style={{ margin: '20px', cursor: 'pointer' }} onClick={() => openPost(_id)} key={_id}>
+                <Typography gutterBottom variant='h6'>{title}</Typography>
+                <Typography gutterBottom variant='subtitle2'>{name}</Typography>
+                <Typography gutterBottom variant='subtitle2'>{message}</Typography>
+                <Typography gutterBottom variant='subtitle1'>Likes: {likes.length}</Typography>
+                <img src={selectedFile} width='200px'/>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
    </Paper>
   );
 };
